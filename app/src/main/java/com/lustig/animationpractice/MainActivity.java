e2546d26f9e9dd5ad5db62442deb6fe2cd39e718
Michael Lustig <halfjew22@gmail.com>
2015-04-24 12:17:03 -0400

    Updated to work with pre-Lollipop

--- app/src/main/java/com/lustig/animationpractice/MainActivity.java
+++ app/src/main/java/com/lustig/animationpractice/MainActivity.java
@@ -2,10 +2,12 @@ package com.lustig.animationpractice;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -31,6 +33,7 @@ public class MainActivity extends Activity {
     int mInitialRadius;
 
     Boolean isCardVisible = true;
+    Boolean isLollipopOrGreater = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +42,20 @@ public class MainActivity extends Activity {
 
         // All manipulations are going to be done on this cool card
         mView = findViewById(R.id.my_cool_card);
+
+        mView.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        animateCard();
+                    }
+                });
+
+        isLollipopOrGreater = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void revealOrHideCard(View v) {
 
         mCX = (mView.getLeft() + mView.getRight()) / 2;
@@ -51,15 +66,20 @@ public class MainActivity extends Activity {
         // If card is invisible, show card with animation
         if (!isCardVisible) {
 
-            mCardAnimator = ViewAnimationUtils
-                    .createCircularReveal(mView, mCX, mCY, 0, mFinalRadius);
-
             mView.setVisibility(View.VISIBLE);
 
-            // Duration of animation in milliseconds
-            mCardAnimator.setDuration(750);
+            // We can currently only use createCircularReveal in Lollipop+
+            if (isLollipopOrGreater) {
 
-            mCardAnimator.start();
+                mCardAnimator = ViewAnimationUtils
+                        .createCircularReveal(mView, mCX, mCY, 0, mFinalRadius);
+
+                // Duration of animation in milliseconds
+                mCardAnimator.setDuration(750);
+
+                mCardAnimator.start();
+
+            }
 
             // After the animation, card is now visible
             isCardVisible = true;
@@ -73,22 +93,30 @@ public class MainActivity extends Activity {
              * be performed twice?
              */
 
-            // If card is visible, hide card with animation
-            mInitialRadius = mView.getWidth();
+            if (isLollipopOrGreater) {
 
-            mCardAnimator = ViewAnimationUtils
-                    .createCircularReveal(mView, mCX, mCY, mInitialRadius, 0);
+                // If card is visible, hide card with animation
+                mInitialRadius = mView.getWidth();
 
-            // We want to set the visibility of the button to invisible /after/ anim
-            mCardAnimator.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    super.onAnimationEnd(animation);
-                    mView.setVisibility(View.INVISIBLE);
-                }
-            });
+                mCardAnimator = ViewAnimationUtils
+                        .createCircularReveal(mView, mCX, mCY, mInitialRadius, 0);
 
-            mCardAnimator.start();
+                // We want to set the visibility of the button to invisible /after/ anim
+                mCardAnimator.addListener(new AnimatorListenerAdapter() {
+                                              @Override
+                                              public void onAnimationEnd(Animator animation) {
+                                                  super.onAnimationEnd(animation);
+                                                  mView.setVisibility(View.INVISIBLE);
+                                              }
+                                          });
+
+                mCardAnimator.start();
+
+            } else {
+
+                mView.setVisibility(View.INVISIBLE);
+
+            }
 
             isCardVisible = false;
 
@@ -110,20 +138,27 @@ public class MainActivity extends Activity {
         super.onConfigurationChanged(newConfig);
     }
 
-    public void animateCard(View v) {
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void animateCard() {
 
         // If card is visible, animate to next activity
         if (isCardVisible) {
 
             Intent intent = new Intent(this, Activity2.class);
 
-            ActivityOptions options = ActivityOptions
-                    .makeSceneTransitionAnimation(this, mView, "animatedCard");
+            // makeSceneTransitionAnimation is Lollipop and up, check current Android level
+            if (isLollipopOrGreater) {
 
-            startActivity(intent, options.toBundle());
+                ActivityOptions options = ActivityOptions
+                        .makeSceneTransitionAnimation(this, mView, "animatedCard");
 
-//            // Right now, we're just starting the activity. Let's attempt to animate the card
-//            startActivity(new Intent(this, Activity2.class));
+                startActivity(intent, options.toBundle());
+
+            } else {
+
+                startActivity(intent);
+
+            }
 
         // If card is invisible, we have nothing to animate... =\
         } else {
