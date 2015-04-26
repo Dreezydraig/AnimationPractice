8b220ef4203148d559bebdf65897dc11342c0189
Michael Lustig <halfjew22@gmail.com>
2015-04-26 14:27:27 -0400

    Updated to work with pre-Lollipop

---
+++ app/src/main/java/com/lustig/animationpractice/Activity2.java
@@ -0,0 +1,15 @@
+package com.lustig.animationpractice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Activity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activity2);
+    }
+}
